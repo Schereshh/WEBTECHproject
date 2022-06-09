0d2270ecf5c1f22107be0673d47f5ec4c394fafe
Schereshh <seres.tamas@student.ms.sapientia.ro>
2022-06-09 15:28:16 +0300

    Controller loading implemented

---
+++ app/controllers/Basic.php
@@ -0,0 +1,15 @@
+<?php 
+
+    class Basic {
+        public function __construct(){
+            
+        }
+        
+        public function index(){
+            echo "Homepage";
+        }
+
+        public function about(){
+            echo "about";
+        }
+    }
--- app/require.php
+++ app/require.php
@@ -4,3 +4,5 @@
     require_once 'libraries/Controller.php';
     require_once 'libraries/Database.php';
 
+    // Instantiate core class
+    $init = new Core();
